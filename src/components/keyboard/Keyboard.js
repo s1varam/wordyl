196ee38fc352a1f34f4a44682222efd33c3569ea
s1varam <71829512+s1varam@users.noreply.github.com>
2022-02-27 12:22:09 +0530

    keyboard design changes

--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -30,7 +30,7 @@ export default function Keyboard({ state, handleKeyPress }) {
                 return <div className="flex justify-center">
                 {row.map((char, i) => {
                     return <button 
-                                className={`${state && state.correctArray && state.correctArray.includes(char) ? "bg-keyCorrect p-2 m-1 text-lg" : (state && state.presentArray && state.presentArray.includes(char) ? "bg-keyPresent p-2 m-1 text-lg" : (state && state.absentArray && state.absentArray.includes(char) ? "bg-keyAbsent p-2 m-1 text-lg" : "bg-keyboardBg p-2 m-1 text-lg"))}`} 
+                                className={`key-tile font-bold rounded ${state && state.correctArray && state.correctArray.includes(char) ? "bg-keyCorrect p-2 m-1 text-lg" : (state && state.presentArray && state.presentArray.includes(char) ? "bg-keyPresent p-2 m-1 text-lg" : (state && state.absentArray && state.absentArray.includes(char) ? "bg-keyAbsent p-2 m-1 text-lg" : "bg-keyboardBg p-2 m-1 text-lg"))}`} 
                                 onClick={() => handleKeyPress(char)}
                             >
                         {char.toUpperCase()}
