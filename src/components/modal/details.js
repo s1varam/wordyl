07f885d220c5fc40eeadf5cef2cae06e0f0379ed
s1varam <71829512+s1varam@users.noreply.github.com>
2022-02-27 22:31:14 +0530

    Update details.js

--- src/components/modal/details.js
+++ src/components/modal/details.js
@@ -9,7 +9,7 @@ export default function Details(props) {
         <>
             {<div className="bg-slate-200 dark:bg-black dark:text-white bg-opacity-90 dark:bg-opacity-90 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 text-sm select-none z-50">
                 <div className="bg-white dark:bg-neutral-900 p-4 opacity-100 rounded-md text-center w-10/12 xl:w-96 flex flex-col justify-center items-center">
-                    <div>Made with <FontAwesomeIcon icon={faHeart} className="inline ml-1 mr-1 w-3 h-3 text-red-500" /> using React and TailwindCSS.</div>
+                    <div>Made with <FontAwesomeIcon icon={faHeart} className="inline ml-1 mr-1 w-3 h-3 text-red-500" /> using React and Tailwind CSS.</div>
                     <FontAwesomeIcon icon={faGithub} className="mt-4 p-2 w-8 h-8 text-black dark:text-white" onClick={() => props.showGithub()}/>
                     <button class="px-3 py-1 rounded-md text-xs border-keyAbsent text-keyAbsent border-solid border mt-4 self-center" onClick={() => props.close()}>CLOSE</button>
                 </div>
